2737f483c57e2f912f81f2082dacc74722149885
Darnel Montero <darnel.montero@gmail.com>
2021-12-23 12:56:39 -0300

    asd

---
+++ src/firebase/credenciales.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBoNeaI0CtrF_RmZcsJoW0G9JVuBCrchNo",
+  authDomain: "despachospudahuel2.firebaseapp.com",
+  projectId: "despachospudahuel2",
+  storageBucket: "despachospudahuel2.appspot.com",
+  messagingSenderId: "610968505969",
+  appId: "1:610968505969:web:717cac64fdcf2189cf15d0"
+};
+
+// Initialize Firebase
+const db = initializeApp(firebaseConfig);
+
+
+export default db;
